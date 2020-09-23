d3f482664ff0104835025d204b22dbd3e63d3950
Javier Eliud <javiereliudliz@gmail.com>
2020-09-22 19:26:02 -0700

    cubito

---
+++ Assets/Scripts/ARTracking.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARTracking : MonoBehaviour
+{
+    [SerializeField]
+    GameObject srcObject;
+
+    GameObject spawnedObj;
+    ARRaycastManager aRRaycastManager;
+    Vector2 touchPos;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    
+    void Awake() 
+    {
+        aRRaycastManager = GetComponent<ARRaycastManager>();    
+    }
+
+    void Update()
+    {
+        if(isTouching(out Vector2 touchPos))
+        {
+            if(aRRaycastManager.Raycast(touchPos, hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose pose = hits[0].pose;
+                    if(!spawnedObj)
+                    {
+                        spawnedObj = Instantiate(srcObject, pose.position, pose.rotation);
+                    }
+                    else
+                    {
+                        spawnedObj.transform.position = pose.position;
+                        spawnedObj.transform.rotation = pose.rotation;                    }
+            }
+        }
+    }
+
+    bool isTouching(out Vector2 touchPos)
+    {
+        if(Input.touchCount > 0){
+            touchPos = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPos = default;
+        return false;
+    }
+
+}
